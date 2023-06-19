b750847f952284a3e0d6a99026975845f1134cd8
John Y. Pazekha <neboskreb@narod.ru>
2023-06-19 08:47:50 +0200

    Added action to connect OpenVPN

---
+++ open-vpn-connect/await-connection.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TARGET_IP=$1
+if [ -z $TARGET_IP ]
+then
+    echo "FATAL: Missing required parameter TARGET_IP" >&2
+    exit 1
+fi
+
+echo "INFO: Target IP $TARGET_IP" >&2
+
+for (( i=10; ; i-- ))
+do
+  PING_MESSAGE=$(ping -c1 -t1 "$TARGET_IP")
+  PING_RESULT=$?
+  if [ $PING_RESULT -eq 0 ]
+  then
+    echo "INFO: Connected successfully" >&2
+    exit 0
+  fi
+
+  if [[ $i -le 0 ]]
+  then
+    break
+  fi
+  echo "INFO: Not connected, retries left: $i" >&2
+  sleep 1
+done
+echo "Last ping message:" >&2
+echo "$PING_MESSAGE" >&2
+echo "FATAL: Connection timeout elapsed" >&2
+exit 1
